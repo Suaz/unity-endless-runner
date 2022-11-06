493038ff5bf853abfc0b0b2b041b82b55b0b6bc7
Carlos Suaznabar <carlos.sznbr@gmail.com>
2022-11-06 03:03:16 -0400

    Basic scenary

---
+++ Assets/Scripts/CityBlockController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityBlockController : MonoBehaviour
+{
+
+    private int speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, speed * -Time.deltaTime));
+
+        if(transform.position.z < -12)
+        {
+            Destroy(this.gameObject);
+            LevelController.Instance.AddBlockAtEnd();
+        }
+    }
+}
---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] GameObject[] cityBlocks;
+    [SerializeField] int minBlock = 3;
+
+    public static LevelController Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    void Start()
+    {
+        for(int i=0; i< minBlock; i++)
+        {
+            AddBlock(i);
+        }
+    }
+
+    public void AddBlockAtEnd()
+    {
+        AddBlock(minBlock - 1);
+    }
+
+    public void AddBlock(int position)
+    {
+
+        Debug.Log(position);
+        Instantiate(cityBlocks[0], new Vector3(0, 0, position * 12), Quaternion.identity);
+    }
+}
